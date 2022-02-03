49024b2ce70bc88b7c4211ba338049349999b063
Mariia Shaf <mashashef1212@gmail.com>
2022-02-03 14:16:30 +0200

    fix: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let res = '';
+  let a = bracketsConfig;
+  let strN = str
+  for(i = 0; i<strN.length; i++){
+ for( let key in a){
+   let c = str.indexOf(a[key][0]);
+   let b = str.indexOf(a[key][1]);
+    let del = a[key][0]+a[key][1];
+    for (u = 0; u < strN.length; u++){
+    if (str.includes(a[key][0]+a[key][1])){
+      str = str.replace(del, '');
+      u = 0;
+    } if(str  === ''){u = strN.length}}
+   if (str === ''){res = true}
+   else{res = false};
+    if(str  === ''){break}
+ }
+     if(str  === ''){i = strN.length}
 }
+  return res;
+}
+
